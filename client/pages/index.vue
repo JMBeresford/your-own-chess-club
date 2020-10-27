0cd9dc6e68c1e0dcef2129cc24620af9b77ebd09
root <root@dev-chessclub.dc.rr.com>
2020-10-27 21:37:32 +0000

    nuxt refactor

---
+++ client/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-container id="mainContainer" fluid>
+    <b-row>
+      <b-form id="login" @submit="login">
+        <h3>Login</h3>
+        <b-form-group>
+          
+        </b-form-group>
+      </b-form>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+  body {
+    background-color: var(--dark);
+  }
+  #login {
+    background-color: var(--light);
+  }
+</style>
